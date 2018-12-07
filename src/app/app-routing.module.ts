d26f8fa756f4ba724e30fe2eeda7981ecc253dba
groman-gonzalez <gerald.romangonzalez@gmail.com>
2018-12-06 19:50:57 -0500

    Created a bookstore angular application, that list all books and authors. You can update the list by clicking on either of the author or book

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'detail', component: DetailComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
